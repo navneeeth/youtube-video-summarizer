97198eb9b007845b60b1c90a28b8039d9465e615
navneeeth <navneeth.padaki15@gmail.com>
2023-03-24 21:41:30 -0400

    Initial commit.

---
+++ back-end/server.py
@@ -0,0 +1,194 @@
+from flask import Flask, request, jsonify
+import random
+import string
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import moviepy.editor as mp
+import openai
+from pytube import YouTube
+import threading
+import requests
+from flask_cors import CORS
+from validators import url as is_valid_url
+
+
+app = Flask(__name__)
+CORS(app)
+MAX_THREADS = 5
+active_threads = []
+
+# Initialize Firebase credentials and Firestore client
+cred = credentials.Certificate('videosummarizergpt.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def process_request(new_id, timestamp, video_link, video_title):
+    print('Started thread')
+    print(threading.current_thread().name)
+    db.collection('auth').document(new_id).set({'timestamp': timestamp, 'status': 'Downloading', 'video_link': video_link, 'video_title': video_title, 'summary': ''})
+    video_file = video_title + ".mp4"
+    audio_file = video_title + ".mp3"
+    updated_audio_file = "updated_" + audio_file
+    youtube = YouTube(video_link)
+    print(youtube)
+    audio = youtube.streams.filter(only_audio=True).first()
+    audio.download(filename=video_file)
+    # convert the downloaded audio file to mp3 format
+    mp.AudioFileClip(video_file).write_audiofile(audio_file)
+    print("Processing finished for timestamp:", timestamp, "and video link:", video_link)
+    db.collection('auth').document(new_id).set({'timestamp': timestamp, 'status': 'Transcribing', 'video_link': video_link, 'video_title': video_title, 'summary': ''})
+    # transcribe the audio using OpenAI's API
+    file = open(audio_file, "rb")
+    transcription = openai.Audio.transcribe("whisper-1", file)
+
+    # write the transcription to a text file
+    with open(video_title+"_transcription.txt", "w", encoding="utf-8") as f:
+        f.write(transcription["text"])
+    db.collection('auth').document(new_id).set({'timestamp': timestamp, 'status': 'Summarizing', 'video_link': video_link, 'video_title': video_title, 'summary': ''})
+    prompt = "Organize this transcription from a YouTube video into a structured set of easily understandable points without missing important details: "+transcription["text"]
+    summary = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", 
+        messages=[{"role": "user", "content": prompt}]
+    )
+    text = ""
+    print(summary.choices)
+    if len(summary.choices) > 0:
+        text = summary.choices[0].message.content
+        print(text)
+    else:
+        print("Error: No response generated.")
+        db.collection('auth').document(new_id).set({'timestamp': timestamp, 'status': 'Error', 'video_link': video_link, 'video_title': video_title, 'summary': ''})
+    # print the summary and write it to a text file
+    with open(video_title+"_summary.txt", "w") as f:
+        f.write(text)
+    db.collection('auth').document(new_id).set({'timestamp': timestamp, 'status': 'Ready', 'video_link': video_link, 'video_title': video_title, 'summary': text})
+    os.remove(video_title+"_summary.txt")
+    os.remove(video_title+"_transcription.txt")
+    os.remove(video_file)
+    os.remove(audio_file)
+    active_threads.stop()
+
+def start_processing(new_id, timestamp, video_link, video_title):
+    global active_threads
+    # Check if there are already MAX_THREADS active threads
+    if len(active_threads) >= MAX_THREADS:
+        # Wait for one of the threads to complete
+        active_threads[0].join()
+        # Remove the completed thread from the list
+        active_threads = active_threads[1:]
+    # Create a new thread for the request
+    t = threading.Thread(target=process_request, args=(new_id, timestamp, video_link, video_title))
+    # Add the thread to the list of active threads
+    active_threads.append(t)
+    # Start the thread
+    t.start()
+
+@app.after_request
+def add_header(response):
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM *'
+    return response
+
+@app.route('/')
+def hello():
+    return "Hello!"
+@app.route('/get-id', methods=['POST'])
+def get_id():
+    request_data = request.get_json()
+    timestamp = request_data.get('timestamp')
+    video_link = request_data.get('videoLink')
+    video_title = request_data.get('videoTitle')
+    acknowledgement_id = None
+
+    while not acknowledgement_id:
+        # Generate a random 15-character ID with numbers and letters
+        new_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))
+
+        # Check if the ID already exists in the Firestore collection
+        id_doc = db.collection('auth').document(new_id).get()
+        if not id_doc.exists:
+            # If the ID is unique, store it in the Firestore collection
+            db.collection('auth').document(new_id).set({'timestamp': timestamp, 'status': 'Acknowledged', 'video_link': video_link, 'video_title': video_title, 'summary': ''})
+            acknowledgement_id = new_id
+    #data = [new_id, timestamp, video_link, video_title]
+    start_processing(new_id, timestamp, video_link, video_title)
+    # Return the acknowledgement ID to the user
+    print(acknowledgement_id)
+    return jsonify({
+        'ack_id': acknowledgement_id,
+        'status': 'success'
+    })
+    
+
+
+# Route to get the status of the processing task
+@app.route('/get-status', methods=['POST'])
+def get_status():
+    # Get the acknowledgment ID from the JSON request
+    data = request.get_json()
+    ack_id = data.get('ackId')
+
+    # Retrieve the status from Firebase
+    doc_ref = db.collection(u'auth').document(ack_id)
+    status = doc_ref.get().to_dict()['status']
+
+    # Return the status
+    return jsonify({
+        'status': status
+    })
+
+# Route to get the summary of the processed task
+@app.route('/get-summary', methods=['POST'])
+def get_summary():
+    # Get the acknowledgment ID from the JSON request
+    data = request.get_json()
+    ack_id = data.get('ackId')
+    
+    # Retrieve the status from Firebase
+    doc_ref = db.collection(u'auth').document(ack_id)
+    summary = doc_ref.get().to_dict()['summary']
+    
+    # Return the status
+    return jsonify({
+        'status': 'success',
+        'message': summary
+    })
+
+@app.route('/validate-openai-id', methods=['POST'])
+def validate_openai_id():
+    openai_id = request.form['openai-id']
+    print(openai_id)
+    try:
+        openai.api_key = openai_id
+        models = openai.Model.list()
+        #print(models)
+        #print(type(models))
+    
+        print('success')
+        return jsonify({'status': 'success'})
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+
+@app.route('/get-title')
+def get_title():
+    video_link = request.args.get('videoLink')
+    if not is_valid_url(video_link):
+        print('Invalid YouTube link:', video_link)
+        return jsonify({'status': 'error', 'message': 'Please enter a valid YouTube link'})
+    
+    response = requests.get(f'https://www.youtube.com/oembed?url={video_link}&format=json')
+    if response.ok:
+        print('Valid YouTube link:', video_link)
+        title = response.json()['title']
+        return jsonify({'status': 'success', 'message': title})
+    else:
+        print('Invalid YouTube link:', video_link)
+        return jsonify({'status': 'error', 'message': 'Could not retrieve video title'})
+
+
+# Example usage
+if __name__ == '__main__':
+    app.run()
+    
---
+++ front-end/script.js
@@ -0,0 +1,272 @@
+const apiUrl = "https://yt-video-summarizer-backend.herokuapp.com";
+var formattedTitle = '';
+function previewVideo() {
+    const videoLink = document.getElementById("video-link").value;
+    const iframe = document.getElementById("video-iframe");
+    const title = document.getElementById("video-title");
+    var thumbnailUrl = '';
+    
+    var thumbnail;
+    
+    
+
+
+    const openaiId = document.getElementById("openai-id").value;
+    if (videoLink == "" || openaiId == "") {
+        alert("Please enter both a YouTube video link and an OpenAI API key.");
+      } 
+
+    // Set the src attribute of the iframe to the entered video link
+    iframe.setAttribute("src", videoLink);
+    // if(validateOpenAIID(openaiId) == true) 
+
+  validateOpenAIID(openaiId).then(result => {
+    if (result) {
+        // continue with video summary process
+        console.log('OpenAI ID is valid');
+         // Make an API call to get the video title
+    fetch(`${apiUrl}/get-title?videoLink=${videoLink}`)
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
+            // Display the video preview and title
+            document.getElementById("status-text").innerHTML = "";
+            document.getElementById("status").style.display = "none";
+            iframe.setAttribute('src', `https://www.youtube.com/embed/${videoLink.substring(videoLink.lastIndexOf('/') + 1)}`);
+            document.getElementById("video-preview").style.display = "block";
+            formattedTitle = data.message.replace(/\s+/g, "_");
+            // Remove special characters
+            formattedTitle = formattedTitle.replace(/[^\w\s]/gi, "");
+            title.innerHTML = data.message;
+            console.log(title.innerHTML);
+            
+        } else {
+            // Display an error message
+            document.getElementById("status-text").innerHTML = "Please enter a valid YouTube link";
+            document.getElementById("status").style.display = "block";
+        }
+    })
+    .catch(error => {
+        // Display an error message
+        document.getElementById("status-text").innerHTML = "An error occurred while getting the video title. Please try again later.";
+        document.getElementById("status").style.display = "block";
+    });
+    } else {
+        // handle OpenAI ID validation error
+    }
+});
+
+        
+  
+   
+}
+
+function goBack() {
+    // Hide the video preview and show the form again
+    document.getElementById("video-preview").style.display = "none";
+    document.getElementById("status").style.display = "none";
+}
+
+/*
+function continueProcess() {
+    // Show the loading status and make an API call to get the video summary
+    document.getElementById("status-text").innerHTML = "Loading...";
+    document.getElementById("status").style.display = "block";
+    const videoLink = document.getElementById("video-link").value;
+    //const videoTitle = document.getElementById("video-title").innerHTML;
+    var videoTitle = formattedTitle;
+    const timestamp = Date.now();
+    
+    fetch(`${apiUrl}/get-id`, {
+        method: "POST",
+        body: JSON.stringify({
+            videoLink: videoLink,
+            videoTitle: videoTitle,
+            timestamp: timestamp
+        }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
+            // Call the 'get-status' API every 3 seconds
+            const ackId = data.ack_id;
+            console.log('success in get id');
+            console.log(ackId);
+            let interval = setInterval(() => {
+                fetch(`${apiUrl}/get-status?ackId=${ackId}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        document.getElementById("status-text").innerHTML = data.status;
+                        if (data.status === "Ready") {
+                            clearInterval(interval);
+                            fetch(`${apiUrl}/get-summary?ackId=${ackId}`)
+                                .then(response => response.json())
+                                .then(data => {
+                                    if (data.status === "success") {
+                                        // Display the video summary
+                                        document.getElementById("status").style.display = "none";
+                                        document.getElementById("result-text").innerHTML = data.message;
+                                        document.getElementById("result").style.display = "block";
+                                    } else {
+                                        // Display an error message
+                                        document.getElementById("status-text").innerHTML = "An error occurred while getting the video summary.";
+                                    }
+                                })
+                                .catch(error => {
+                                    // Display an error message
+                                    document.getElementById("status-text").innerHTML = "An error occurred while getting the video summary.";
+                                });
+                        } else if (data.status === "Error") {
+                            console.log("Error in status of get id");
+                            clearInterval(interval);
+                            // Display an error message
+                            document.getElementById("status-text").innerHTML = "An error occurred while processing the video.";
+                        }
+                    })
+                    .catch(error => {
+                        console.log("Error in catch of get status");
+                        clearInterval(interval);
+                        // Display an error message
+                        document.getElementById("status-text").innerHTML = "An error occurred while checking the status.";
+                    });
+            }, 3000);
+        } else {
+            // Display an error message
+            console.log("Error in else success false of get id");
+            document.getElementById("status-text").innerHTML = "An error occurred while processing the video.";
+        }
+    })
+    .catch(error => {
+        // Display an error message
+        console.log("Error in catch of get id");
+        document.getElementById("status-text").innerHTML = "An error occurred while processing the video.";
+    });
+}
+*/
+
+function continueProcess() {
+    // Show the loading status and make an API call to get the video summary
+    document.getElementById("status-text").innerHTML = "Loading...";
+    document.getElementById("status").style.display = "block";
+    const videoLink = document.getElementById("video-link").value;
+    //const videoTitle = document.getElementById("video-title").innerHTML;
+    var videoTitle = formattedTitle;
+    const timestamp = Date.now();
+    
+    fetch(`${apiUrl}/get-id`, {
+        method: "POST",
+        body: JSON.stringify({
+            videoLink: videoLink,
+            videoTitle: videoTitle,
+            timestamp: timestamp
+        }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
+            // Call the 'get-status' API every 3 seconds
+            const ackId = data.ack_id;
+            console.log('success in get id');
+            console.log(ackId);
+            let interval = setInterval(() => {
+                fetch(`${apiUrl}/get-status`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        ackId: ackId
+                    }),
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById("status-text").innerHTML = data.status;
+                    if (data.status === "Ready") {
+                        clearInterval(interval);
+                        fetch(`${apiUrl}/get-summary`, {
+                            method: "POST",
+                            body: JSON.stringify({
+                                ackId: ackId
+                            }),
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.status === "success") {
+                                // Display the video summary
+                                document.getElementById("status").style.display = "none";
+                                document.getElementById("result-text").innerHTML = data.message;
+                                document.getElementById("result").style.display = "block";
+                            } else {
+                                // Display an error message
+                                document.getElementById("status-text").innerHTML = "An error occurred while getting the video summary.";
+                            }
+                        })
+                        .catch(error => {
+                            // Display an error message
+                            document.getElementById("status-text").innerHTML = "An error occurred while getting the video summary.";
+                        });
+                    } else if (data.status === "Error") {
+                        console.log("Error in status of get id");
+                        clearInterval(interval);
+                        // Display an error message
+                        document.getElementById("status-text").innerHTML = "An error occurred while processing the video.";
+                    }
+                })
+                .catch(error => {
+                    console.log("Error in catch of get status");
+                    clearInterval(interval);
+                    // Display an error message
+                    document.getElementById("status-text").innerHTML = "An error occurred while checking the status.";
+                });
+            }, 3000);
+        } else {
+            // Display an error message
+            console.log("Error in else success false of get id");
+            document.getElementById("status-text").innerHTML = "An error occurred while processing the video.";
+        }
+    })
+    .catch(error => {
+        // Display an error message
+        console.log("Error in catch of get id");
+        document.getElementById("status-text").innerHTML = "An error occurred while processing the video.";
+    });
+}
+
+
+
+function validateOpenAIID(openaiId) {
+    return fetch(`${apiUrl}/validate-openai-id`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `openai-id=${openaiId}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+            // Continue with video summary process
+            console.log('success validating open ai id');
+            return true;
+        } else {
+            alert('OpenAI ID invalid');
+            return false;
+        }
+    })
+    .catch(error => {
+        console.log('An error occurred while validating OpenAI ID', error);
+        alert('An error occurred while validating OpenAI ID. Please try again later.');
+        return false;
+    });
+}
+
+
